51136019f8b093707c3d6a425293e0c054b47a85
Elena Titenko <ellena.titenko@gmail.com>
2023-05-25 16:22:33 +0300

    small fixes

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,7 +1,3 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-// import { getFirestore } from "firebase/firestore";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth"
 import 'firebase/compat/database'
