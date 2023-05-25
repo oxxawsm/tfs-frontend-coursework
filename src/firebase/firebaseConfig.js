6ea5e49dc3a30b5a134b7431fe24a268ae3a3064
Elena Titenko <ellena.titenko@gmail.com>
2023-05-25 16:21:53 +0300

    small fixes

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -7,30 +7,11 @@ import "firebase/compat/auth"
 import 'firebase/compat/database'
 
 
-// import configureStore from "../store";
 
-// import { getAnalytics } from "firebase/analytics";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgHRsovluzb78IWKS0PunV8AtCBdfK6dQ",
-  authDomain: "oxxawsm-coursework.firebaseapp.com",
-  projectId: "oxxawsm-coursework",
-  storageBucket: "oxxawsm-coursework.appspot.com",
-  messagingSenderId: "74832170748",
-  appId: "1:74832170748:web:620ae25b241f09e70a1179",
-  measurementId: "G-HBZTMZSTMS"
+  // config code
 };
-
-// export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app)
-// export const Firebase = getFirestore(app);
-
 
 
 export const Firebase = firebase.initializeApp(firebaseConfig);
